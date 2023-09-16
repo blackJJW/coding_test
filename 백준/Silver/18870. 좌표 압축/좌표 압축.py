de30855131ee54ef5ff351e1abb68e45ca24fea6
Jinwoo Jung <jjinwoo92@gmail.com>
2023-09-16 19:46:34 +0900

    [Silver II] Title: 좌표 압축, Time: 1832 ms, Memory: 154800 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,8 @@
+N = int(input())
+input_list = list(map(int, input().split()))
+
+sorted_list = sorted(list(set(input_list)))
+dict_list = dict(zip(sorted_list, list(range(len(sorted_list)))))
+    
+for x in input_list:
+    print(dict_list[x])
