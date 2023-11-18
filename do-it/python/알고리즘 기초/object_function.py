fa8968742dc6e92a3712812ad8846f7895064e97
blackJJW <jjinwoo92@gmail.com>
2023-11-19 00:25:20 +0900

    object

---
+++ do-it/python/알고리즘 기초/object_function.py
@@ -0,0 +1,11 @@
+# 함수 내부, 외부에서 정의한 변수와 객체의 식별 번호 출력
+
+n = 1 # 전역 변수
+
+def put_id():
+    x = 1 # 지역 변수
+    print(f'id(x) = {id(x)}')
+    
+print(f'id(1) = {id(1)}')
+print(f'id(n) = {id(n)}')
+put_id()
